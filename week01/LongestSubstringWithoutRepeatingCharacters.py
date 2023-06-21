7fd0c7f3c88be1164eacc7efb6e7ab2285a35d63
BrendonEarl <63430919+BrendonEarl@users.noreply.github.com>
2023-06-21 18:36:34 +0000

    format

--- week01/LongestSubstringWithoutRepeatingCharacters.py
+++ week01/LongestSubstringWithoutRepeatingCharacters.py
@@ -29,51 +29,49 @@ Constraints:
 0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces.
 """
+
+
 class Solution:
-    
+
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max,longest,longest_length = len(s),[],0
-        for index,_ in enumerate(s):
+        max, longest, longest_length = len(s), [], 0
+        for index, _ in enumerate(s):
             chars = []
             ll = 0
             spoint = index
             while True:
-                if spoint>=max:
-                    if ll>longest_length:
-                        longest_length=ll
-                        longest=chars
-                    break 
-                if (a:=s[spoint]) not in chars:
+                if spoint >= max:
+                    if ll > longest_length:
+                        longest_length = ll
+                        longest = chars
+                    break
+                if (a := s[spoint]) not in chars:
                     chars.append(a)
                     ll += 1
                     spoint += 1
                 else:
-                    if ll>longest_length:
-                        longest_length=ll
-                        longest=chars
+                    if ll > longest_length:
+                        longest_length = ll
+                        longest = chars
                     break
-        print(f'chars: {longest} \n length: {longest_length}') 
-        return longest_length  
-    
+        print(f'chars: {longest} \n length: {longest_length}')
+        return longest_length
+
     def lengthOfLongestSubstringSpeed(self, s: str) -> int:
-        longest_length,spoint,epoint,longest = 0,0,0,[]
-        
+        longest_length, spoint, epoint, longest = 0, 0, 0, []
+
         visited = {}
-        for epoint in range(len(s)):
-            char = s[epoint]
-            if char in visited and (new_spoint:=visited[char]+1) > spoint:
+        for epoint, char in enumerate(s):
+            if char in visited and (new_spoint := visited[char]+1) > spoint:
                 spoint = new_spoint
             else:
                 if (new_longest_length := epoint-spoint+1) > longest_length:
                     longest_length = new_longest_length
                     longest = s[spoint:epoint+1]
-                    
-                
-            visited[char]=epoint
-        print(f'chars: {longest} \n length: {longest_length}') 
-        return longest_length
-                    
-            
-        
 
-# Solution().lengthOfLongestSubstringSpeed("pwwkew")
+            visited[char] = epoint
+        print(f'chars: {longest} \n length: {longest_length}')
+        return longest_length
+
+
+Solution().lengthOfLongestSubstringSpeed("pwwkew")
