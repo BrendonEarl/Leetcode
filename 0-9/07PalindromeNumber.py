5ab42c6157cb1558f72a3c03d58ed4baa8fbd6ff
BrendonEarl <63430919+BrendonEarl@users.noreply.github.com>
2023-06-23 18:21:18 +0000

    works without converting to string

--- 0-9/07PalindromeNumber.py
+++ 0-9/07PalindromeNumber.py
@@ -2,11 +2,28 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x<0:
             return False
+
+        counter, t = -1, x
+        while (t):
+            t = t//10
+            counter += 1
         
-        x = str(x)
+        w, t = 0, x
+        for i in range(counter,-1,-1):
+            w += (t%10)*(10**i)
+            t = t//10
+            
+        return w==x
         
         
-        return x == x[-1::-1]
-    
+"""
+def isPalindrome(self, x: int) -> bool:
+    if x<0:
+        return False
+    x = str(x)
+    return x == x[-1::-1]
+"""
+
 #next try with pointers
 # then use mods to avoid converting to string
+Solution().isPalindrome(x = 666)
