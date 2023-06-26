7df695fd08c2734197fd4041faf0ba06e2ca9720
BrendonEarl <63430919+BrendonEarl@users.noreply.github.com>
2023-06-26 21:53:35 +0000

    day 10 & 11

--- 0-9/07PalindromeNumber.py
+++ 0-9/07PalindromeNumber.py
@@ -1,29 +1,19 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x<0:
+        if x < 0:
             return False
 
         counter, t = -1, x
-        while (t):
+        while t:
             t = t//10
             counter += 1
-        
-        w, t = 0, x
-        for i in range(counter,-1,-1):
-            w += (t%10)*(10**i)
-            t = t//10
-            
-        return w==x
-        
-        
-"""
-def isPalindrome(self, x: int) -> bool:
-    if x<0:
-        return False
-    x = str(x)
-    return x == x[-1::-1]
-"""
 
-#next try with pointers
-# then use mods to avoid converting to string
-Solution().isPalindrome(x = 666)
+        w, t = 0, x
+        for i in range(counter, -1, -1):
+            w += (t % 10)*(10**i)
+            t = t//10
+
+        return w == x
+
+
+Solution().isPalindrome(x=666)
---
+++ 10-11/10LongestCommonPrefix.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        ans = strs[0]
+        for _, s in enumerate(strs):
+            if s == ans:
+                continue
+            no_eq = min((slen := len(s)), (alen := len(ans)))
+            small = min(slen, alen)
+            for i in range(small):
+                if s[i] != ans[i]:
+                    no_eq = i
+                    break
+            ans = ans[0:no_eq]
+        return ans
+
+
+print(Solution().longestCommonPrefix(["ag", "a"]))
