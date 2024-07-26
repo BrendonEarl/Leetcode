93bec48e3d7bb57f87a1cc19ef63a372b1c018c3
brendon earl <brendon@brendons-MacBook-Pro.local>
2024-07-26 09:26:40 -0600

    need to learn more about hash tables

---
+++ 26MajorityElement.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        done = []
+
+        for num in nums:
+            if (num not in done) and (nums.count(num) > len(nums)/2):
+                return num
+            done.append(num)
+
+    #todo: hash table
