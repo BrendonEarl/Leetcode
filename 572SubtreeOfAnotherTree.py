67a168b1fb70f98f1481377a6121560dd2b310a8
brendon earl <brendon@brendons-MacBook-Pro.local>
2024-08-06 11:03:32 -0600

    finish later

--- 572SubtreeOfAnotherTree.py
+++ 572SubtreeOfAnotherTree.py
@@ -6,4 +6,9 @@ class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         pass
 
+
+tn = TreeNode()
+test = tn.getRootFromArray([3, 4, 5, 1, 2, None, None, None, None, 0])
+print(test)
+# todo: finish l8r
 # https://leetcode.com/problems/subtree-of-another-tree
--- DataStructures/TreeNode.py
+++ DataStructures/TreeNode.py
@@ -3,3 +3,17 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+    def getRootFromArray(self, arr):
+        if not arr:
+            return None
+        return self.__getRootFromArray(0, arr)
+
+    def __getRootFromArray(self, index, arr):
+        if index >= len(arr):
+            return None
+        node = TreeNode(arr[index])
+        node.left = self.__getRootFromArray((index * 2) + 1, arr)
+        node.right = self.__getRootFromArray((index * 2) + 2, arr)
+
+        return node
