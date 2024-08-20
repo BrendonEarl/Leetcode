0f32a9d5f53697ec3c9b9a65737b2ceb0edf7822
brendon earl <brendon@brendons-MacBook-Pro.local>
2024-08-20 13:22:01 -0600

    binary tree wip

--- 543DiameterOfBinaryTree.py
+++ 543DiameterOfBinaryTree.py
@@ -4,24 +4,25 @@ from DataStructures.TreeNode import TreeNode
 
 class Solution:
     longest = 0
+
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.diameterHelper(root)
         return self.longest
+
     def diameterHelper(self, node):
         if node is None:
             return 0
 
         l = 1 + self.diameterHelper(node.left)
         r = 1 + self.diameterHelper(node.right)
-        if (x:=(l+r-2)) > self.longest:
+        if (x := (l + r - 2)) > self.longest:
             self.longest = x
 
-        return max(l,r)
+        return max(l, r)
 
 
-
-troot = TreeNode().getRootFromArray([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,None,None,5,None,None,None,None,None,9])
+troot = TreeNode().getRootFromArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, -15, None, 9])
+print(troot)
 s = Solution().diameterOfBinaryTree(troot)
-print(s)
 
 # https://leetcode.com/problems/diameter-of-binary-tree
--- DataStructures/TreeNode.py
+++ DataStructures/TreeNode.py
@@ -5,8 +5,8 @@ class TreeNode:
         self.right = right
 
     def __str__(self):
-        return (f'{self.val} | \n'
-                f'L: {str(self.left)}         R: {str(self.right)}')
+        # return f'{self.val}'
+        return self.__str_help()
 
     def getRootFromArray(self, arr):
         if not arr:
@@ -14,10 +14,54 @@ class TreeNode:
         return self.__getRootFromArray(0, arr)
 
     def __getRootFromArray(self, index, arr):
-        if index >= len(arr):
+        if index >= len(arr) or (arr[index] is None):
             return None
         node = TreeNode(arr[index])
         node.left = self.__getRootFromArray((index * 2) + 1, arr)
         node.right = self.__getRootFromArray((index * 2) + 2, arr)
 
         return node
+
+    def __str_help(self):
+        mxdigits = self.__max_digits__()
+        depth = self.__depth__()
+        width = self.__width__()
+        return str((mxdigits, depth, width))
+    def __max_digits__(self):
+        return self.__max_digits_help(self)
+
+    def __max_digits_help(self, node):
+        if node is None:
+            return 0
+
+        l = self.__max_digits_help(node.left)
+        r = self.__max_digits_help(node.right)
+
+        x = len(str(node.val))
+
+        return max(l, r, x)
+
+    def __width_help(self, node, pos, lr):
+        if node is None:
+            return lr
+
+        lr = (min(pos, lr[0]), max(pos, lr[1]))
+        l = self.__width_help(node.left, pos - 1, lr)
+        r = self.__width_help(node.right, pos + 1, lr)
+
+        return min(pos, l[0], r[0]), max(pos, l[1], r[1])
+
+    def __width__(self):
+
+        l, r = self.__width_help(self, 0, (0, 0))
+        return (l, r)
+
+    def __depth__(self):
+        
+        return self.__depth_help(self)
+
+    def __depth_help(self, node):
+        if node is None:
+            return 0
+
+        return 1 + max(self.__depth_help(node.left), self.__depth_help(node.right))
