fa834d7070bca000e733c92b80a1de8c25fa8ff7
brendon earl <brendon@brendons-MacBook-Pro.local>
2025-05-19 10:00:23 -0600

    add binary

--- 067AddBinary.py
+++ 067AddBinary.py
@@ -1,15 +1,38 @@
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        long, short =  (b[::-1], a[::-1]) if len(b) > len(a) else (a[::-1] , b[::-1])
 
-        one,two = a[::-1],b[::-1]
-        r = 0
-        i = 0
-        x = ''
-        while i < len(one) or i < len(two):
-            if i < len(one) and i < len(two):
-                if one[i] and two[i]:
-                    pass
+        remainder = 0
+        index = 0
+        backwards = ''
 
-# todo complete
+        while index < len(long):
+            
+            digit1 = long[index]
+            digit2 = short[index] if index < len(short) else '0'
+            add = 0
+            if digit1 == '1':
+                add += 1
+            if digit2 == '1':
+                add += 1
+            if remainder > 0:
+                add += 1
+                remainder -= 1
+            if add > 1:
+                remainder += 1
+
+            if add % 2:
+                backwards += '1'
+            else:
+                backwards += '0'
+
+
+            index+=1
+        if remainder > 0:
+            backwards += '1'
+
+        return backwards[::-1]
+
+
 Solution().addBinary(a = "1010", b = "1011")
